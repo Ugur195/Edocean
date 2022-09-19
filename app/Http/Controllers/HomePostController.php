f5937e458eff04e393e391e6b2f6268b0ec4b575
Rzaev125 <rzaev125@mail.ru>
2022-09-19 01:05:20 -0700

    Register Change

--- app/Http/Controllers/HomePostController.php
+++ app/Http/Controllers/HomePostController.php
@@ -34,45 +34,44 @@ class HomePostController extends Controller
 
     public function PostSignUp(Request $request)
     {
-        try {
-            $request->validate([
-                'fin' => 'required',
-                'name' => 'required',
-                'email' => 'required',
-                'password' => 'required|min:6',
-            ]);
+        $request->validate([
+            'fin' => 'required',
+            'name' => 'required',
+            'email' => 'required',
+            'password' => 'required|min:6',
+        ]);
 
-            $userforfin = User::where('fin', $request->fin)->first();
-            $userforemail = User::where('email', $request->email)->first();
+        $userforfin = User::where('fin', $request->fin)->first();
+        $userforemail = User::where('email', $request->email)->first();
 
-            if ($userforfin == null && $userforemail == null) {
-                $user = new User();
-                $user->fin = $request->fin;
-                $user->author = $request->radio;
-                $user->name = $request->name;
-                $user->email = $request->email;
-                $user->password = Hash::make($request->password);
-                $user->slug = Str::slug($request->name);
-                $user->status = 0;
-                $user->save();
-
-                Mail::send('emails.mesaj_gonder', ['msg' => 'Message: ' . '<a href="aa.com">Qeydiyyatdan ugurlu kecdiz</a>'], function ($message) use ($request) {
-                    $message->to($request->email, $request->name)->subject('Mail linki');
-                    $message->from('edocean_course@mail.ru', 'Edocean Course');
-                    $message->setBody('<a href="/aaaaa.com">salam</a>', 'text/html');
-                });
-                return response(['title' => 'Ugurlu!', 'message' => 'Qeydiyyatdan ugurlu kecdiz', 'status' => 'success']);
-
-            } else if ($userforfin !== null) {
-                return response(['title' => 'Ugursuz!', 'message' => 'Bele fin artiq movcudur', 'status' => 'error']);
-            } else {
-                return response(['title' => 'Ugursuz!', 'message' => 'Bele email artiq movcudur', 'status' => 'error']);
-            }
-
-        } catch (\Exception $exception) {
-            return response(['title' => 'Ugursuz!', 'message' => 'Qeydiyyatdan kecmek mumkun olmadi', 'status' => 'error']);
+        if ($userforfin == null && $userforemail == null) {
+            $user = new User();
+            $user->fin = $request->fin;
+            $user->author = $request->radio;
+            $user->name = $request->name;
+            $user->email = $request->email;
+            $user->password = Hash::make($request->password);
+            $user->slug = Str::slug($request->name);
+            $user->status = 0;
+            $user->save();    
+                
+            Mail::send('emails.mesaj_gonder', ['msg' => 'Message: ' . '<a href="aa.com">Qeydiyyatdan ugurlu kecdiz</a>'], function ($message) use ($request) {
+                $message->to($request->email, $request->name)->subject('Mail linki');
+                $message->from('edocean_course@mail.ru', 'Edocean Course');
+                $message->setBody('<a href="/aaaaa.com">salam</a>', 'text/html');
+            });
+            return response(['title' => 'Ugurlu!', 'message' => 'Qeydiyyatdan ugurlu kecdiz', 'status' => 'success']);
+        } 
+        else if ($userforfin !== null) {
+            return response(['title' => 'Ugursuz!', 'message' => 'Bele fin artiq movcudur', 'status' => 'error']);
+        } 
+        else if ($userforemail !== null) {
+            return response(['title' => 'Ugursuz!', 'message' => 'Bele email artiq movcudur', 'status' => 'error']);
         }
-
+        else
+        {
+            return response(['title' => 'Ugursuz!', 'message' => 'Qeydiyyatdan kecmek mumkun olmadi', 'status' => 'error']);
+        }    
     }
 
     public function PostSignIn()
