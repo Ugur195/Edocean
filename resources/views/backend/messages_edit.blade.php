83868bcc0d15ad7affa79fe14f6b7d222169b16d
Rzaev125 <rzaev125@mail.ru>
2022-10-19 22:59:24 -0700

    Cosmetic_3

--- resources/views/backend/messages_edit.blade.php
+++ resources/views/backend/messages_edit.blade.php
@@ -4,8 +4,7 @@
 @endsection
 
 @section('content')
-    <div class="content d-flex flex-column flex-column-fluid" id="kt_content">
-
+    <div class="d-flex flex-column flex-column-fluid" id="kt_content">
         <div class="d-flex flex-column-fluid">
             <div class="container">
                 <div class="card card-custom gutter-b">
