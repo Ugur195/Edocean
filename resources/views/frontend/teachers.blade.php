ef83dfd878c77013dbeeaa3eb45c51f410c8bf48
Ugur Meherremov <ugur-212@mail.ru>
2022-11-21 11:59:46 +0400

    FrontendBlogsChange4

--- resources/views/frontend/teachers.blade.php
+++ resources/views/frontend/teachers.blade.php
@@ -21,42 +21,43 @@
                 <div class="row">
                     <div class="col-lg-8 col-md-12 col-sm-12 mb-5">
                         @foreach ($teacher as $t)
-                        <div class="col-lg-6 col-md-6 col-sm-12 blog-card-container">
-                            <div class="blog-card">
-                            <img class="teacher-card-img" src="data:image/jpeg;base64,{{base64_encode($t->image)}}" alt="">
-                            <div class="details">
-                                <div class="details-1">
-                                    <div class="rating-and-views">
-                                        <p>
-                                            <ion-icon name="eye"></ion-icon>
-                                            {{ $t->see_count }}
-                                        </p>
-                                        <p>
-                                            <ion-icon name="star"></ion-icon>
-                                            4.6(50)
-                                        </p>
+                            <div class="col-lg-6 col-md-6 col-sm-12 blog-card-container">
+                                <div class="blog-card">
+                                    <img class="teacher-card-img"
+                                         src="data:image/jpeg;base64,{{base64_encode($t->image)}}" alt="">
+                                    <div class="details">
+                                        <div class="details-1">
+                                            <div class="rating-and-views">
+                                                <p>
+                                                    <ion-icon name="eye"></ion-icon>
+                                                    {{ $t->see_count }}
+                                                </p>
+                                                <p>
+                                                    <ion-icon name="star"></ion-icon>
+                                                    4.6(50)
+                                                </p>
+                                            </div>
+                                            <h5>{{ $t->speciality }}</h5>
+                                            <div class="name-and-img">
+                                                <img src="./frontendCss/images/kanan.png" alt="">
+                                                <p>{{ $t->name.' '.$t->surname }}</p>
+                                            </div>
+                                        </div>
+                                        <div class="date-and-button">
+                                            <p>
+                                                <ion-icon name="calendar-number"></ion-icon>
+                                                {{ $t->created_at }}
+                                            </p>
+                                            <a href="">
+                                                <button>Daha Ətraflı
+                                                    <ion-icon name="arrow-forward"></ion-icon>
+                                                </button>
+                                            </a>
+                                        </div>
                                     </div>
-                                    <h5>{{ $t->speciality }}</h5>
-                                    <div class="name-and-img">
-                                        <img src="./frontendCss/images/kanan.png" alt="">
-                                        <p>{{ $t->name.' '.$t->surname }}</p>
-                                    </div>
-                                </div>
-                                <div class="date-and-button">
-                                    <p>
-                                        <ion-icon name="calendar-number"></ion-icon>
-                                        {{ $t->created_at }}
-                                    </p>
-                                    <a href="">
-                                        <button>Daha Ətraflı
-                                            <ion-icon name="arrow-forward"></ion-icon>
-                                        </button>
-                                    </a>
                                 </div>
                             </div>
-                        </div>
-                        </div>
-                @endforeach
+                        @endforeach
                     </div>
                     <div class="col-lg-4 col-md-12 col-sm-12 blog-main-right-container">
                         <div class="recent-posts">
